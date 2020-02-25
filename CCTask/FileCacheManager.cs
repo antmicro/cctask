9b0c5c011852fbfbb7e5e23634d102600e491b3b
Mateusz Karlic <mkarlic@internships.antmicro.com>
2020-02-25 15:21:53 +0100

    [#16767] Fix updating hash too soon

--- CCTask/FileCacheManager.cs
+++ CCTask/FileCacheManager.cs
@@ -13,7 +13,7 @@ namespace CCTask
 		public FileCacheManager(string directory = null)
 		{
 			hasherSource = new ThreadLocal<MD5>(() => MD5.Create());
-			hashDb = new Dictionary<Tuple<string, string>, string>();
+			hashDb = new Dictionary<DictionaryKey, DictionaryContent>();
 
 			hashDbFile = Path.Combine(directory ?? Directory.GetCurrentDirectory(), HashDbFilename);
 			Directory.CreateDirectory(Path.GetDirectoryName(hashDbFile));
@@ -55,7 +55,7 @@ namespace CCTask
 				foreach(var line in File.ReadLines(hashDbFile))
 				{
 					var fileAndHash = line.Split(new [] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-					hashDb.Add(Tuple.Create(fileAndHash[0], fileAndHash[1]), fileAndHash[2]);
+					hashDb.Add(new DictionaryKey(fileAndHash[0], fileAndHash[1]), new DictionaryContent(fileAndHash[2], false));
 				}
 			}
 		}
@@ -65,7 +65,7 @@ namespace CCTask
 			Directory.CreateDirectory(Path.GetDirectoryName(hashDbFile));
 			lock(hashDb)
 			{
-				File.WriteAllLines(hashDbFile, hashDb.Select(x => string.Format("{0};{1};{2}", x.Key.Item1, x.Key.Item2, x.Value)));
+				File.WriteAllLines(hashDbFile, hashDb.Select(x => string.Format("{0};{1};{2}", x.Key.Path, x.Key.Arguments, x.Value.Hash)));
 			}
 		}
 
@@ -83,20 +83,25 @@ namespace CCTask
 			}
 
 			var result = false;
-			var hashKey = Tuple.Create(sourcePath, args);
+			var hashKey = new DictionaryKey(sourcePath, args);
+			var hashEntry = new DictionaryContent();
 			lock(hashDb)
 			{
-				if(!hashDb.ContainsKey(hashKey))
+				if(!hashDb.TryGetValue(hashKey, out hashEntry))
 				{
 					result = true;
 				}
+				else if(hashEntry.Modified)
+				{
+					return true;
+				}
 				else
 				{
-					result = (hashDb[hashKey] != hash);
+					result = (hashDb[hashKey].Hash != hash);
 				}
 				if(result)
 				{
-					hashDb[hashKey] = hash;
+					hashDb[hashKey] = new DictionaryContent(hash, true);
 				}
 #if DEBUG
 				Console.WriteLine("Check of file {0} resulted in {1}, key {2}, hash {3}, in db {4}", sourcePath, result, hashKey, hash, hashDbFile);
@@ -105,9 +110,31 @@ namespace CCTask
 			return result;
 		}
 
-		private readonly Dictionary<Tuple<string, string>, string> hashDb;
+		private readonly Dictionary<DictionaryKey, DictionaryContent> hashDb;
 		private readonly string hashDbFile;
 		private readonly ThreadLocal<MD5> hasherSource;
+
+		private struct DictionaryKey
+		{
+			public DictionaryKey(string path, string arguments)
+			{
+				this.Path = path;
+				this.Arguments = arguments;
+			}
+			public string Path;
+			public string Arguments;
+		}
+
+		private struct DictionaryContent
+		{
+			public DictionaryContent(string hash, bool modified)
+			{
+				this.Hash = hash;
+				this.Modified = modified;
+			}
+			public string Hash;
+			public bool Modified;
+		}
 
 		private const string HashDbFilename = "hashes";
 	}
